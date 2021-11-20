650a961026f5f68fa570eff6ff81df04fd80eeb3
Jiayin Pei <pjymymartin@gmail.com>
2021-11-20 19:27:07 +0800

    update

--- Package.swift
+++ Package.swift
@@ -13,11 +13,11 @@ let package = Package(
             targets: ["TunnelRayBinary"]),
     ],
     dependencies: [
-//        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
-//        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
-//        .package(url: "https://github.com/Kitura/BlueRSA.git", from: "1.0.201"),
-//        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
-//        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
+        .package(url: "https://github.com/Kitura/BlueRSA.git", from: "1.0.201"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
